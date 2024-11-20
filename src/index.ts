36cb13207886fcc92765433618f886d89228da27
iamAyanBiswas <ayanbiswas9191@gmail.com>
2024-11-20 22:49:53 +0530

    fix error

--- src/index.ts
+++ src/index.ts
@@ -4,45 +4,48 @@ import { generateKeyAuth, getKeyValue, encryption, decryption } from "./lib/func
 import { deleteDB } from "./lib/indexDB.lib";
 
 
-function encStorage() {
-    let generateKey = async (): Promise<void> => {
-        let randomNumbet: number = randomIntegerInRange(10, 20)
-        let keyValue: string = randomString(randomNumbet)
-        await generateKeyAuth("__primaryKey", keyValue)
-    }
-    let deleteKey =async():Promise<void>=>{
-        await deleteDB("__encDB")
-    }
-    let getItem = async (item: string, storageType: string = 'localstorage'): Promise<any> => {
-        if (storageType !== 'localstorage' || storageType !== 'localstorage') throw new Error("Invalid storageType ")
-        let encData: string;
-        storageType === "localstorage" ? encData = localStorage.getItem(item) : encData = sessionStorage.getItem(item)
-        if (encData === '') return ""
-        else {
+const encStorage = ():any => {
+    return{
+        generateKey : async (): Promise<void> => {
+            let randomNumbet: number = randomIntegerInRange(10, 20)
+            let keyValue: string = randomString(randomNumbet)
+            await generateKeyAuth("__primaryKey", keyValue)
+        },
+        deleteKey : async():Promise<void>=>{
+            await deleteDB("__encDB")
+        },
+        getItem : async (item: string, storageType: string = 'localstorage'): Promise<any> => {
+            if (storageType !== 'localstorage' || storageType !== 'localstorage') throw new Error("Invalid storageType ")
+            let encData: string;
+            storageType === "localstorage" ? encData = localStorage.getItem(item) : encData = sessionStorage.getItem(item)
+            if (encData === '') return ""
+            else {
+                try {
+                    let encriptionKey: string = await getKeyValue("__primaryKey")
+                    let values: any=decryption(encData,encriptionKey)
+                    return values
+                } catch (_error) {
+                    throw _error
+                }
+            }
+        },
+        setItem : async (item: string, value: string, storageType: string = 'localstorage'): Promise<void> => {
+            if (storageType !== 'localstorage' || storageType !== 'localstorage') throw new Error("Invalid storageType ")
+    
             try {
                 let encriptionKey: string = await getKeyValue("__primaryKey")
-                let values: any=decryption(encData,encriptionKey)
-                return values
+                //enc part
+                let encData = encryption(value, encriptionKey)
+                storageType === "localstorage" ? localStorage.setItem(item, encData) : sessionStorage.setItem(item, encData)
+    
             } catch (_error) {
                 throw _error
             }
         }
     }
-    let setItem = async (item: string, value: string, storageType: string = 'localstorage'): Promise<void> => {
-        if (storageType !== 'localstorage' || storageType !== 'localstorage') throw new Error("Invalid storageType ")
-
-        try {
-            let encriptionKey: string = await getKeyValue("__primaryKey")
-            //enc part
-            let encData = encryption(value, encriptionKey)
-            storageType === "localstorage" ? localStorage.setItem(item, encData) : sessionStorage.setItem(item, encData)
-
-        } catch (_error) {
-            throw _error
-        }
-    }
-    return { generateKey, deleteKey, getItem, setItem }
+    
+    
 }
 
 
-
+export default encStorage
